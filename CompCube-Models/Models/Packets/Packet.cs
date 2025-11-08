6856cbc47a7df103481e16966a0b58b02b100d4d
Bela Varga <100267928+NotBela@users.noreply.github.com>
2025-11-08 18:06:01 -0500

    added deserialize method

--- CompCube-Models/Models/Packets/Packet.cs
+++ CompCube-Models/Models/Packets/Packet.cs
@@ -6,4 +6,9 @@ namespace CompCube_Models.Models.Packets;
 public abstract class Packet
 {
     public byte[] SerializeToBytes() => Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(this));
+
+    public T? Deserialize<T>(string data)
+    {
+        return JsonConvert.DeserializeObject<T>(data);
+    }
 }
