8b343ebb9355296dcb33622c59e9eb36fad4316f
Bela Varga <100267928+NotBela@users.noreply.github.com>
2025-12-01 18:32:03 -0500

    more progress on redoing matches

--- CompCube-Models/Models/Match/MatchResultsData.cs
+++ CompCube-Models/Models/Match/MatchResultsData.cs
@@ -2,12 +2,11 @@
 
 namespace CompCube_Models.Models.Match;
 
-public class MatchResultsData(MatchScore winner, MatchScore loser, int mmrChange, VotingMap? map, bool premature, int id, DateTime time)
+public class MatchResultsData(string[] winners, string[] losers, int mmrChange, bool premature, int id, DateTime time)
 {
-    public readonly MatchScore Winner = winner;
-    public readonly MatchScore Loser = loser;
+    public readonly string[] Winner = winners;
+    public readonly string[] Loser = losers;
     public readonly int MmrChange = mmrChange;
-    public readonly VotingMap? Map = map;
     public readonly bool Premature = premature;
     public readonly int Id = id;
     public readonly DateTime Time = time;
