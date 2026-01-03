2dfea5a0d368e1fe884c269a0c030eb737f9d64d
Bela Varga <100267928+NotBela@users.noreply.github.com>
2026-01-03 02:22:53 -0500

    add contributor parsing

--- CompCube-Models/Models/Contributors/Contributor.cs
+++ CompCube-Models/Models/Contributors/Contributor.cs
@@ -1,8 +1,15 @@
-﻿namespace CompCube_Server.Models.CompCube_Models.Models.Contributors;
+﻿using Newtonsoft.Json;
+
+namespace CompCube_Server.Models.CompCube_Models.Models.Contributors;
 
 public class Contributor(string name, string role, string profilePictureLink)
 {
-    public readonly string Name = name;
-    public readonly string Role = role;
-    public readonly string ProfilePictureLink = profilePictureLink;
+    [JsonProperty("name")]
+    public string Name => name;
+    
+    [JsonProperty("role")]
+    public string Role => role;
+    
+    [JsonProperty("pfpLink")]
+    public string ProfilePictureLink => profilePictureLink;
 }
