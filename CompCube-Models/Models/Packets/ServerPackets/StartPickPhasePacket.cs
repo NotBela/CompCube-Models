0de6ed2c90b0afe3f0b2b049e0ced53cc7e5d6c3
Bela Varga <100267928+NotBela@users.noreply.github.com>
2026-07-05 21:59:56 -0700

    added field for whos pick it is

--- CompCube-Models/Models/Packets/ServerPackets/StartPickPhasePacket.cs
+++ CompCube-Models/Models/Packets/ServerPackets/StartPickPhasePacket.cs
@@ -4,9 +4,11 @@ using Newtonsoft.Json;
 namespace CompCube_Models.Models.Packets.ServerPackets;
 
 [method: JsonConstructor]
-public class StartPickPhasePacket(VotingMap[] availableMaps) : ServerPacket
+public class StartPickPhasePacket(VotingMap[] availableMaps, bool redPick) : ServerPacket
 {
     public override ServerPacketTypes PacketType => ServerPacketTypes.StartPickPhase;
 
     public readonly VotingMap[] AvailableMaps = availableMaps;
+
+    public readonly bool RedPick = redPick;
 }
