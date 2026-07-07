2d840102e7052edb6d75316c6991a5357f7311e6
Bela Varga <100267928+NotBela@users.noreply.github.com>
2026-07-06 20:43:42 -0700

    change downloading songs to be key based instead of hash based

--- CompCube-Models/Models/Map/VotingMap.cs
+++ CompCube-Models/Models/Map/VotingMap.cs
@@ -4,13 +4,13 @@ namespace CompCube_Models.Models.Map;
 
 [method: JsonConstructor]
 public class VotingMap(
-    string hash,
+    string key,
     VotingMap.DifficultyType difficulty,
     VotingMap.Category category,
     string categoryLabel)
 {
     [JsonProperty("hash")]
-    public readonly string Hash = hash;
+    public readonly string Key = key;
 
     [JsonProperty("difficulty")]
     public readonly DifficultyType Difficulty = difficulty;
