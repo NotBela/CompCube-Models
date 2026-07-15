018588fc826416fe82ff2f627cfff9ece2a20a1a
Bela <27pvarga@gmail.com>
2026-07-15 19:14:59 -0400

    update how packets are deserialized

--- CompCube-Models/Models/Packets/UserPacket.cs
+++ CompCube-Models/Models/Packets/UserPacket.cs
@@ -11,30 +11,32 @@ public abstract class UserPacket : Packet
     
     public static bool TryDeserialize(string json, out UserPacket? packet)
     {
-        var jobj = JObject.Parse(json);
+        try
+        {
+            var jobj = JObject.Parse(json);
 
-        if (!jobj.TryGetValue("type", out var packetTypeJToken))
+            if (!jobj.TryGetValue("type", out var packetTypeJToken) || !Enum.TryParse<UserPacketTypes>(packetTypeJToken.ToObject<string>(), out var userPacketType))
+            {
+                packet = null;
+                return false;
+            }
+
+            packet = userPacketType switch
+            {
+                UserPacketTypes.JoinRequest => JsonConvert.DeserializeObject<JoinRequestPacket>(json)!,
+                UserPacketTypes.MapSelection => JsonConvert.DeserializeObject<MapSelectionPacket>(json)!,
+                UserPacketTypes.ScoreSubmission => JsonConvert.DeserializeObject<ScoreSubmissionPacket>(json)!,
+                UserPacketTypes.DiscardMaps => JsonConvert.DeserializeObject<DiscardMapsPacket>(json)!,
+                _ => throw new Exception("Could not get packet type!")
+            };
+
+            return true;
+        }
+        catch
         {
             packet = null;
             return false;
         }
-
-        if (!Enum.TryParse<UserPacketTypes>(packetTypeJToken.ToObject<string>(), out var userPacketType))
-        {
-            packet = null;
-            return false;
-        }
-
-        packet = userPacketType switch
-        {
-            UserPacketTypes.JoinRequest => JsonConvert.DeserializeObject<JoinRequestPacket>(json)!,
-            UserPacketTypes.MapSelection => JsonConvert.DeserializeObject<MapSelectionPacket>(json)!,
-            UserPacketTypes.ScoreSubmission => JsonConvert.DeserializeObject<ScoreSubmissionPacket>(json)!,
-            UserPacketTypes.DiscardMaps => JsonConvert.DeserializeObject<DiscardMapsPacket>(json)!,
-            _ => throw new Exception("Could not get packet type!")
-        };
-
-        return true;
     }
 
     public enum UserPacketTypes
